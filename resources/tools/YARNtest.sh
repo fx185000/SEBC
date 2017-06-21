821ca79bb2d3b0d40e0538d608bc09475346c1fe
fx185000 <feng.xu@cloudera.com>
2017-06-20 17:01:17 -0700

    put yarn png files

--- resources/tools/YARNtest.sh
+++ resources/tools/YARNtest.sh
@@ -3,19 +3,23 @@
 
 MR=/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce
 HADOOP=/opt/cloudera/parcels/CDH/bin
+count=0
 
 # Mark start of the loop
 echo Testing loop started on `date`
 
 # Mapper containers
-for i in 8    
+for i in 1 2 4 8    
 do
    # Reducer containers
-   for j in 1 
+   for j in 1 2 4 8 
    do                 
       # Container memory
       for k in 512 1024 
       do                         
+         
+         count=$(($count+1))
+         echo "$count:  Mapper=$i   Reducer=$j  memory=$k start..."
          # Set mapper JVM heap 
          MAP_MB=`echo "($k*0.8)/1" | bc` 
 
@@ -26,7 +30,7 @@ do
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
-                     51200000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
+                     51200000 /user/fx185000/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
 
        time ${HADOOP}/hadoop jar $MR/hadoop-examples.jar terasort \
                      -Dmapreduce.job.maps=$i \
@@ -35,11 +39,12 @@ do
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      -Dmapreduce.reduce.memory.mb=$k \
                      -Dmapreduce.reduce.java.opts.max.heap=$RED_MB \
-	             /results/tg-10GB-${i}-${j}-${k}  \
-                     /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
+	             /user/fx185000/tg-10GB-${i}-${j}-${k}  \
+                     /user/fx185000/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
 
-        $HADOOP/hadoop fs -rm -r -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
-        $HADOOP/hadoop fs -rm -r -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
+        $HADOOP/hadoop fs -rm -r -skipTrash /user/fx185000/tg-10GB-${i}-${j}-${k}                         
+        $HADOOP/hadoop fs -rm -r -skipTrash /user/fx185000/ts-10GB-${i}-${j}-${k} 
+        echo "$count: done!"                
       done
    done
 done
